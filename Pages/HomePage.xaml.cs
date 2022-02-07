9f59fd9632597d6775b9e98bf432cbe0472021cc
Ирина Дорошенко <irina.doroshenko@jetsoftpro.com>
2022-02-07 21:49:54 +0200

    Added Bindings and CollectionView

--- Pages/HomePage.xaml.cs
+++ Pages/HomePage.xaml.cs
@@ -2,19 +2,10 @@
 
 public partial class HomePage : ContentPage
 {
-	int count = 0;
 
-	public HomePage()
-	{
-		InitializeComponent();
-	}
-
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-		CounterLabel.Text = $"Current count: {count}";
-
-		SemanticScreenReader.Announce(CounterLabel.Text);
-	}
+    public HomePage()
+    {
+        InitializeComponent();
+    }
 }
 
--- ViewModels/HomePageModel.cs
+++ ViewModels/HomePageModel.cs
@@ -1,13 +1,45 @@
 ﻿using System;
 using FreshMvvm.Maui;
+using HelloWorldMaui.Models;
 
 namespace HelloWorldMaui.ViewModels
 {
     public class HomePageModel : FreshBasePageModel
     {
+        private List<ItemModel> _itemsList;
+        public List<ItemModel> ItemsList
+        {
+            get
+            {
+                return _itemsList;
+            }
+            set
+            {
+                if (_itemsList != value)
+                {
+                    _itemsList = value;
+                    RaisePropertyChanged(nameof(ItemsList));
+                }
+            }
+        }
+
         public HomePageModel()
         {
-            
+            UpdateItems();
+        }
+
+        private void UpdateItems()
+        {
+            ItemsList = new();
+
+            for (int i = 0; i < 101; i++)
+            {
+                ItemsList.Add(new ItemModel()
+                {
+                    Name = $"Item # {i} added",
+                    IsChecked = i % 2 == 0
+                });
+            }
         }
     }
 }
